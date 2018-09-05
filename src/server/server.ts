0663df4bcbc3b44bdcab7f41aaf2446b5444396c
Jed Thompson <jedster1111@hotmail.co.uk>
2018-09-06 00:15:26 +0100

    Set up tslint for typescript linting

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
 import express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+import bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -10,6 +10,4 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.json({ message: "hooray! welcome to our api" }));
 
-
 app.listen(3000, () => console.log("App is running on port 3000"));
-
