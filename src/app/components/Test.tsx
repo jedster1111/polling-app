b3740b67ed6cc269eda7dff4bcbad33151ff5786
Jed Thompson <jedster1111@hotmail.co.uk>
2018-09-12 16:19:52 +0100

    Styled components is installed and I have a very basic example working

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
+import styled from "styled-components";
 
-const Test = () => <div>Wow storybook</div>;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: green;
+  width: 250px;
+  height: 50px;
+  font-size: 25px;
+`;
+
+const Test = () => <Wrapper>Wow storybook</Wrapper>;
 
 export default Test;
