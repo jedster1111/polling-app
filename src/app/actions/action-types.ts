588a86cf434317c993e115f08dc12fb94669c5f0
Jed Thompson <jedster1111@hotmail.co.uk>
2018-09-12 11:53:54 +0100

    Removed unused action

--- src/app/actions/action-types.ts
+++ src/app/actions/action-types.ts
@@ -1,5 +1,4 @@
 export const ADD_ARTICLE = "ADD_ARTICLE";
-export const ADD_POLL = "ADD_POLL";
 
 export const GET_POLLS_REQUEST = "GET_POLLS_REQUEST";
 export const GET_POLLS_SUCCESS = "GET_POLLS_SUCCESS";
--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,14 +1,10 @@
 import { Action, ActionCreator } from "redux";
-import { Poll, PollInput } from "../../../server/models/database";
+import { PollInput } from "../../../server/models/database";
 import { Article } from "../reducers/reducers";
 import * as actionTypes from "./action-types";
 export const addArticle: ActionCreator<Action> = (article: Article) => ({
   type: actionTypes.ADD_ARTICLE,
   payload: article
-});
-export const addPoll: ActionCreator<Action> = (poll: Poll) => ({
-  type: actionTypes.ADD_POLL,
-  payload: poll
 });
 export const fetchPolls: ActionCreator<Action> = () => ({
   type: actionTypes.GET_POLLS_REQUEST
