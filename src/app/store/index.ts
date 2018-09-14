efc091921e4bc950467f5d7493bc988bde0203a0
Jed Thompson <jedster1111@hotmail.co.uk>
2018-09-14 15:32:16 +0100

    Fixed issue where redux dev tools was required to avoid crashing

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,19 +1,14 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers/rootReducer";
 import { mainSaga } from "../sagas/sagas";
 
 const sagaMiddleware = createSagaMiddleware();
-const reduxDevTools =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    reduxDevTools
-  )
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(mainSaga);
