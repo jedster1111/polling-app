62a5b4ef83f9d1ffa5cba303ef72af14955cb593
Jed Thompson <jedster1111@hotmail.co.uk>
2018-09-17 14:30:50 +0100

    React Router is now hooked up to the redux store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,14 +1,20 @@
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers/rootReducer";
 import { mainSaga } from "../sagas/sagas";
 
+export const history = createBrowserHistory();
+
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
+  connectRouter(history)(rootReducer),
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware, routerMiddleware(history))
+  )
 );
 
 sagaMiddleware.run(mainSaga);
