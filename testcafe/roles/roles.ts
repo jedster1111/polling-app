1069b672d8fc5daf428faaf27b9f91863495931e
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-04 16:41:00 +0100

    Created conditional Selector to handle reauthorization scenario

--- testcafe/roles/roles.ts
+++ testcafe/roles/roles.ts
@@ -1,4 +1,4 @@
-import { ClientFunction, Role } from "testcafe";
+import { ClientFunction, Role, Selector } from "testcafe";
 import * as githubUser from "../../secret/githubTestAuth";
 
 const getPageUrl = ClientFunction(() => window.location.href.toString());
@@ -7,7 +7,10 @@ export const githubTestUser = Role("localhost:8000/auth/github", async t => {
   await t
     .typeText("#login_field", githubUser.username)
     .typeText("#password", githubUser.password)
-    .pressKey("enter")
-    .expect(getPageUrl())
-    .contains("127.0.0.1:8000", { timeout: 10000 });
+    .pressKey("enter");
+  if (await Selector("#js-oauth-authorize-btn").exists) {
+    await t.click("#js-oauth-authorize-btn");
+  } else {
+    await t.expect(getPageUrl()).contains("127.0.0.1:8000", { timeout: 10000 });
+  }
 });
