ea80fd4ad29334bd31afa1e522b805a28847897e
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-11 01:36:26 +0100

    Started building create poll form with antd

--- src/app/components/create-poll-form/OptionsInput.tsx
+++ src/app/components/create-poll-form/OptionsInput.tsx
@@ -1,29 +1,6 @@
+import { Button, Form, Input } from "antd";
 import * as React from "react";
-import styled from "styled-components";
-import OptionButton from "./AddRemoveOptionButton";
-import Label from "./PollFormLabel";
-import { SingleInputContainer } from "./SingleInput";
-import StyledTextInput from "./TextInput";
 
-const StyledOptionsInputContainer = styled.div<{}>`
-  flex: 1;
-  flex-wrap: wrap;
-  display: flex;
-  max-width: 600px;
-`;
-const StyledOptionTextInput = styled(StyledTextInput)`
-  flex: 1 1 100%;
-  margin-left: 0;
-  margin-right: 0;
-`;
-StyledOptionTextInput.displayName = "StyledOptionTextInput";
-const SingleOptionInputContainer = styled.div<{}>`
-  flex: 1 1 100%;
-  display: flex;
-  flex-wrap: nowrap;
-  margin: 0;
-  align-items: center;
-`;
 interface OptionsInputProps {
   values: Array<{ optionId: string; value: string }>;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -32,34 +9,48 @@ interface OptionsInputProps {
   edit?: boolean;
 }
 
-const StyledButton = styled(OptionButton)`
-  margin-left: auto;
-`;
-
 const OptionsInput = (props: OptionsInputProps) => {
   return (
-    <SingleInputContainer>
-      <Label>Options: </Label>
-      <StyledOptionsInputContainer>
-        {props.values.map((option, index) => (
-          <SingleOptionInputContainer key={index}>
-            <StyledOptionTextInput
-              id={`optionInput${index + 1}`}
-              placeholder={option.optionId ? "existing option" : "new option"}
-              value={option.value}
-              handleChange={props.handleChange}
+    <React.Fragment>
+      {props.values.map((option, index) => (
+        // <SingleOptionInputContainer key={index}>
+        //   <StyledOptionTextInput
+        //     id={`optionInput${index + 1}`}
+        //     placeholder={option.optionId ? "existing option" : "new option"}
+        //     value={option.value}
+        //     handleChange={props.handleChange}
+        //   />
+        //   {!option.optionId && (
+        //     <OptionButton
+        //       remove
+        //       onClick={() => props.removePollOption(index)}
+        //     />
+        //   )}
+        // </SingleOptionInputContainer>
+        <Form.Item
+          key={index}
+          label={index === 0 ? "Options" : ""}
+          labelCol={{ span: 3 }}
+          wrapperCol={{ span: 19, offset: index === 0 ? 0 : 3 }}
+        >
+          <Input
+            id={`optionInput${index + 1}`}
+            value={option.value}
+            placeholder={option.optionId ? "existing option" : "new option"}
+            onChange={props.handleChange}
+            style={{ marginRight: 8 }}
+          />
+          {!option.optionId && (
+            <Button
+              key={index}
+              icon="minus-circle"
+              shape="circle"
+              onClick={() => props.removePollOption(index)}
             />
-            {!option.optionId && (
-              <OptionButton
-                remove
-                onClick={() => props.removePollOption(index)}
-              />
-            )}
-          </SingleOptionInputContainer>
-        ))}
-        <StyledButton add onClick={props.addPollOption} />
-      </StyledOptionsInputContainer>
-    </SingleInputContainer>
+          )}
+        </Form.Item>
+      ))}
+    </React.Fragment>
   );
 };
 
--- src/app/components/create-poll-form/PollForm.tsx
+++ src/app/components/create-poll-form/PollForm.tsx
@@ -1,10 +1,7 @@
+import { Button, Form, Input } from "antd";
 import * as React from "react";
-import posed from "react-pose";
-import styled from "styled-components";
 import { PollFormInput } from "../../reducers/rootReducer";
-import Buttons from "./AddDiscardButtons";
 import OptionsInput from "./OptionsInput";
-import SingleInput from "./SingleInput";
 
 interface CreatePollFormProps {
   values: PollFormInput;
@@ -16,48 +13,81 @@ interface CreatePollFormProps {
   edit?: boolean;
 }
 
-export const FormContainer = styled(
-  posed.form({
-    enter: { opacity: 1, y: 0, delayChildren: 150, staggerChildren: 150 },
-    exit: { opacity: 0, y: 30 }
-  })
-)`
-  flex: 1;
-  min-width: 240px;
-  max-width: 750px;
-  border: black solid 1px;
-  margin: 0 auto;
-  margin-bottom: 10px;
-  padding: 0px 5px;
-  border-radius: 8px;
-  background-color: #c6dea6;
-`;
+// export const FormContainer = styled(
+//   posed.form({
+//     enter: { opacity: 1, y: 0, delayChildren: 150, staggerChildren: 150 },
+//     exit: { opacity: 0, y: 30 }
+//   })
+// )`
+//   flex: 1;
+//   min-width: 240px;
+//   max-width: 750px;
+//   border: black solid 1px;
+//   margin: 0 auto;
+//   margin-bottom: 10px;
+//   padding: 0px 5px;
+//   border-radius: 8px;
+//   background-color: #c6dea6;
+// `;
 
-const PollForm = (props: CreatePollFormProps) => (
-  <FormContainer onSubmit={props.handleSubmit} id="createPollForm">
-    <SingleInput
-      id="pollName"
-      value={props.values.pollName}
-      labelText="Poll Name"
-      handleChange={props.handleChange}
-      placeholder="Enter the poll name"
-    />
-    <SingleInput
-      id="description"
-      value={props.values.description}
-      labelText="Description"
-      handleChange={props.handleChange}
-      placeholder="Enter the description"
-    />
-    <OptionsInput
-      handleChange={props.handleChange}
-      values={props.values.options}
-      addPollOption={props.addPollOption}
-      removePollOption={props.removePollOption}
-      edit={props.edit}
-    />
-    <Buttons edit={props.edit} discardPoll={props.discardPoll} />
-  </FormContainer>
-);
+const itemLayout = {
+  labelCol: { span: 3 },
+  wrapperCol: { span: 19 }
+};
+
+const PollForm: React.SFC<CreatePollFormProps> = props => {
+  return (
+    // <FormContainer onSubmit={props.handleSubmit} id="createPollForm">
+    //   <SingleInput
+    //     id="pollName"
+    //     value={props.values.pollName}
+    //     labelText="Poll Name"
+    //     handleChange={props.handleChange}
+    //     placeholder="Enter the poll name"
+    //   />
+    //   <SingleInput
+    //     id="description"
+    //     value={props.values.description}
+    //     labelText="Description"
+    //     handleChange={props.handleChange}
+    //     placeholder="Enter the description"
+    //   />
+    //   <OptionsInput
+    //     handleChange={props.handleChange}
+    //     values={props.values.options}
+    //     addPollOption={props.addPollOption}
+    //     removePollOption={props.removePollOption}
+    //     edit={props.edit}
+    //   />
+    //   <Buttons edit={props.edit} discardPoll={props.discardPoll} />
+    // </FormContainer>
+    <Form onSubmit={props.handleSubmit} id="createPollForm" layout="vertical">
+      <Form.Item {...itemLayout} label="Poll Name">
+        <Input
+          value={props.values.pollName}
+          onChange={props.handleChange}
+          id="pollName"
+        />
+      </Form.Item>
+      <Form.Item label="Description" {...itemLayout}>
+        <Input
+          value={props.values.description}
+          onChange={props.handleChange}
+          id="description"
+        />
+      </Form.Item>
+      <OptionsInput
+        addPollOption={props.addPollOption}
+        removePollOption={props.removePollOption}
+        handleChange={props.handleChange}
+        values={props.values.options}
+      />
+
+      <Button type="primary" block htmlType="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 export default PollForm;
