01593b7ea2e898fb825932486cae5433c9d066a8
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-11 12:28:25 +0100

    Layout updates

--- src/app/components/PollingApp.tsx
+++ src/app/components/PollingApp.tsx
@@ -1,41 +1,17 @@
+import { Layout } from "antd";
 import { ConnectedRouter } from "connected-react-router";
 import * as React from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router";
-import styled from "styled-components";
 import { getUserData } from "../actions/actions";
 import { InitialState } from "../reducers/rootReducer";
 import { history } from "../store/index";
-import OldNavBar from "./NavBar";
 import NavBar from "./navbar/NavbarContainer";
 import CreatePollPage from "./pages/CreatePollPage";
 // import HomePage from "./pages/HomePage";
 import PollsListPage from "./pages/PollsListPage";
 
-const StyledContainer = styled.div<{}>`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-content: flex-start;
-  align-items: flex-start;
-  position: fixed;
-  background-color: lightblue;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: 0;
-  background-color: #dff2d8;
-  color: black;
-  padding: 7px 10px;
-  overflow: auto;
-`;
-const StyledPageContainer = styled.div<{}>`
-  flex: 1;
-  max-width: 1000px;
-  min-width: 250px;
-  /* padding: 5px 5px; */
-`;
+const { Header, Content, Footer } = Layout;
 
 interface PollingAppProps {
   getUserData: () => any;
@@ -60,16 +36,20 @@ class PollingApp extends React.Component<PollingAppProps> {
   render() {
     return (
       <ConnectedRouter history={history}>
-        <StyledContainer>
-          <NavBar />
-          <OldNavBar />
-          <Switch>
-            <StyledPageContainer>
-              <Route path="/" exact component={PollsListPage} />
-              <Route path="/create-poll" component={CreatePollPage} />
-            </StyledPageContainer>
-          </Switch>
-        </StyledContainer>
+        <Layout>
+          <Header style={{ background: "#fff" }}>
+            <NavBar />
+          </Header>
+          <Content>
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+              <Switch>
+                <Route path="/" exact component={PollsListPage} />
+                <Route path="/create-poll" component={CreatePollPage} />
+              </Switch>
+            </div>
+          </Content>
+          <Footer />
+        </Layout>
       </ConnectedRouter>
     );
   }
