5c899504a655e54540c4505a996ae5f15a74c906
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-12 18:07:03 +0100

    Got basic antd table working. No interactivity yet

--- src/app/components/poll-detail/PollDetail.tsx
+++ src/app/components/poll-detail/PollDetail.tsx
@@ -1,13 +1,54 @@
+import { Avatar, Card, Table } from "antd";
+import { ColumnProps } from "antd/lib/table";
 import * as React from "react";
-import { Poll } from "../../types";
+import { Poll, PollOption } from "../../types";
 
 export interface PollDetailProps {
   pollData: Poll | undefined;
   isLoading: boolean;
 }
 
-const PollDetail: React.SFC<PollDetailProps> = ({ pollData, isLoading }) => (
-  <p>{pollData ? pollData.pollId : "Poll doesn't exist"}</p>
-);
+const columns: Array<ColumnProps<PollOption>> = [
+  {
+    title: "Option",
+    dataIndex: "option",
+    key: "option",
+    render: (text, option) => option.value
+  },
+  {
+    title: "Votes",
+    dataIndex: "votes",
+    key: "votes",
+    render: (text, option) => option.votes.length
+  }
+];
+
+const PollDetail: React.SFC<PollDetailProps> = ({ pollData, isLoading }) => {
+  if (!pollData) {
+    return <p>That poll doesn't exist</p>;
+  }
+  const { creator, description, pollName, options } = pollData;
+  return (
+    <Card title={pollName}>
+      <Card.Meta
+        title={description}
+        description={creator.displayName || creator.userName}
+        avatar={creator.photos && <Avatar src={creator.photos[0].value} />}
+        style={{ marginBottom: "15px" }}
+      />
+      <Table
+        columns={columns}
+        dataSource={options}
+        rowKey={option => option.optionId}
+      />
+      {/* <p>
+        {pollId}
+        {options.map(option => (
+          <p>{option.value}</p>
+        ))}
+      </p> */}
+    </Card>
+  );
+};
 
 export default PollDetail;
