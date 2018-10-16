c1a447232f0e1bb97e3e8f0df7e5dd117f4904e9
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-16 17:27:17 +0100

    Created tests for action creators

---
+++ src/app/actions/actions.test.ts
@@ -0,0 +1,150 @@
+import { UpdatePollInput } from "../../../server/types";
+import { Poll, PollInput, UpdatePollInputOption } from "../types";
+import * as types from "./action-types";
+import * as actions from "./actions";
+
+describe("Testing actions:", () => {
+  it("should create an action to fetch polls", () => {
+    const expectedAction = {
+      type: types.GET_POLLS_REQUEST
+    };
+
+    expect(actions.fetchPolls()).toEqual(expectedAction);
+  });
+
+  it("should create an action to create a poll", () => {
+    const pollInput: PollInput = {
+      creatorId: "creatorId",
+      description: "description",
+      options: ["option1", "option2", "option3"],
+      pollName: "pollName"
+    };
+    const expectedAction = {
+      type: types.POST_POLLS_REQUEST,
+      payload: pollInput
+    };
+
+    expect(actions.createPoll(pollInput)).toEqual(expectedAction);
+  });
+
+  it("should create an action to update form data", () => {
+    const formInput = { fieldId: "name", value: "banana" };
+    const expectedAction = { type: types.CHANGE_FORM_DATA, payload: formInput };
+
+    expect(actions.changeFormData(formInput.fieldId, formInput.value)).toEqual(
+      expectedAction
+    );
+  });
+
+  it("should create an action to discard poll form data", () => {
+    const expectedAction = { type: types.DISCARD_FORM_DATA };
+
+    expect(actions.discardPoll()).toEqual(expectedAction);
+  });
+
+  it("should create an action to vote on a poll", () => {
+    const voteInput = {
+      userId: "userId",
+      pollId: "pollId",
+      optionId: "optionId"
+    };
+    const expectedAction = {
+      type: types.VOTE_OPTION_LOADING,
+      payload: voteInput
+    };
+
+    expect(
+      actions.voteOption(voteInput.userId, voteInput.pollId, voteInput.optionId)
+    ).toEqual(expectedAction);
+  });
+
+  it("should create an action to toggle if a result is showing or not", () => {
+    const pollId = "pollId";
+    const expectedAction = {
+      type: types.TOGGLE_SHOW_RESULTS_LOADING,
+      payload: { pollId }
+    };
+
+    expect(actions.toggleShowResults(pollId)).toEqual(expectedAction);
+  });
+
+  it("should create an action to add a form's poll option", () => {
+    const expectedAction = { type: types.ADD_POLL_FORM_OPTION };
+
+    expect(actions.addPollOption()).toEqual(expectedAction);
+  });
+
+  it("should create an action to remove a form's poll option", () => {
+    const index = 1;
+    const expectedAction = {
+      type: types.REMOVE_POLL_FORM_OPTION,
+      payload: { index }
+    };
+
+    expect(actions.removePollOption(index)).toEqual(expectedAction);
+  });
+
+  it("should create an action to delete a poll", () => {
+    const userId = "userId";
+    const pollId = "pollId";
+    const expectedAction = {
+      type: types.DELETE_POLL_LOADING,
+      payload: { userId, pollId }
+    };
+
+    expect(actions.deletePoll(userId, pollId)).toEqual(expectedAction);
+  });
+
+  it("should create an action to show a poll's update form", () => {
+    const pollId = "pollId";
+    const user1 = { id: "1", userName: "userName1" };
+    const poll: Poll = {
+      pollName: "pollName",
+      options: [
+        {
+          optionId: "1",
+          value: "value1",
+          votes: [user1]
+        }
+      ],
+      creator: user1,
+      description: "description",
+      pollId
+    };
+    const expectedAction = {
+      type: types.SHOW_UPDATE_POLL_FORM,
+      payload: { pollId, poll }
+    };
+
+    expect(actions.showUpdatePollForm(pollId, poll)).toEqual(expectedAction);
+  });
+
+  it("should create an action to discard a poll's update form", () => {
+    const expectedAction = { type: types.DISCARD_UPDATE_POLL_FORM };
+    expect(actions.discardUpdatePollForm()).toEqual(expectedAction);
+  });
+
+  it("should create an action to update a poll", () => {
+    const userId = "userId";
+    const pollId = "pollId";
+    const pollName = "pollName";
+    const description = "description";
+    const options: UpdatePollInputOption[] = [
+      { optionId: "1", value: "value1" }
+    ];
+    const updatePollInput: UpdatePollInput = { pollName, description, options };
+    const expectedAction = {
+      type: types.UPDATE_POLL_LOADING,
+      payload: { userId, pollId, updatePollInput }
+    };
+
+    expect(actions.updatePoll(userId, pollId, updatePollInput)).toEqual(
+      expectedAction
+    );
+  });
+
+  it("should createa an action that gets user data ", () => {
+    const expectedAction = { type: types.GET_USER_DATA_LOADING };
+    expect(actions.getUserData()).toEqual(expectedAction);
+  });
+});
--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,11 +1,14 @@
-import { Action, ActionCreator } from "redux";
+import { Action } from "redux";
 import { Poll, PollInput, UpdatePollInput } from "../types";
 import * as actionTypes from "./action-types";
 
-export const fetchPolls: ActionCreator<Action> = () => ({
+export const fetchPolls: () => Action = () => ({
   type: actionTypes.GET_POLLS_REQUEST
 });
-export const createPoll: ActionCreator<Action> = (poll: PollInput) => {
+
+export const createPoll: (
+  pollInput: PollInput
+) => Action & { payload: PollInput } = poll => {
   const cleanedPoll = { ...poll };
   const newOptions = cleanedPoll.options.filter(option => option);
   cleanedPoll.options = newOptions;
@@ -14,9 +17,13 @@ export const createPoll: ActionCreator<Action> = (poll: PollInput) => {
     payload: cleanedPoll
   };
 };
-export const changeFormData: ActionCreator<Action> = (
+
+export const changeFormData: (
   fieldId: string,
   value: string
+) => Action & { payload: { fieldId: string; value: string } } = (
+  fieldId,
+  value
 ) => ({
   type: actionTypes.CHANGE_FORM_DATA,
   payload: {
@@ -24,72 +31,72 @@ export const changeFormData: ActionCreator<Action> = (
     value
   }
 });
-export const discardPoll: ActionCreator<Action> = () => ({
+export const discardPoll: () => Action = () => ({
   type: actionTypes.DISCARD_FORM_DATA
 });
-export const changeUserFormData: ActionCreator<Action> = (
-  fieldId: string,
-  value: string
-) => ({
-  type: actionTypes.CHANGE_USER_FORM_DATA,
-  payload: {
-    fieldId,
-    value
-  }
-});
-export const discardUserFormData: ActionCreator<Action> = (
-  confirmedValues: string
-) => ({
-  type: actionTypes.DISCARD_USER_FORM_DATA,
-  payload: { confirmedValues }
-});
-export const voteOption: ActionCreator<Action> = (
+
+export const voteOption: (
   userId: string,
   pollId: string,
   optionId: string
-) => ({
+) => Action & {
+  payload: { userId: string; pollId: string; optionId: string };
+} = (userId, pollId, optionId) => ({
   type: actionTypes.VOTE_OPTION_LOADING,
   payload: { userId, pollId, optionId }
 });
-export const toggleShowResults: ActionCreator<Action> = (pollId: string) => ({
+
+export const toggleShowResults: (
+  pollId: string
+) => Action & { payload: { pollId: string } } = pollId => ({
   type: actionTypes.TOGGLE_SHOW_RESULTS_LOADING,
   payload: { pollId }
 });
-export const toggleChangingName: ActionCreator<Action> = () => ({
-  type: actionTypes.TOGGLE_CHANGING_NAME
-});
-export const addPollOption: ActionCreator<Action> = () => ({
+
+export const addPollOption: () => Action = () => ({
   type: actionTypes.ADD_POLL_FORM_OPTION
 });
-export const removePollOption: ActionCreator<Action> = (index: number) => ({
+
+export const removePollOption: (
+  index: number
+) => Action & { payload: { index: number } } = index => ({
   type: actionTypes.REMOVE_POLL_FORM_OPTION,
   payload: { index }
 });
-export const deletePoll: ActionCreator<Action> = (
+
+export const deletePoll: (
   userId: string,
   pollId: string
-) => ({
+) => Action & {
+  payload: { userId: string; pollId: string };
+} = (userId, pollId) => ({
   type: actionTypes.DELETE_POLL_LOADING,
   payload: { userId, pollId }
 });
-export const showUpdatePollForm: ActionCreator<Action> = (
+
+export const showUpdatePollForm: (
   pollId: string,
   poll: Poll
-) => ({
+) => Action & { payload: { pollId: string; poll: Poll } } = (pollId, poll) => ({
   type: actionTypes.SHOW_UPDATE_POLL_FORM,
   payload: { pollId, poll }
 });
-export const discardUpdatePollForm: ActionCreator<Action> = () => ({
+
+export const discardUpdatePollForm: () => Action = () => ({
   type: actionTypes.DISCARD_UPDATE_POLL_FORM
 });
-export const updatePoll: ActionCreator<Action> = (
+
+export const updatePoll: (
   userId: string,
   pollId: string,
   updatePollInput: UpdatePollInput
-) => ({
+) => Action & {
+  payload: { userId: string; pollId: string; updatePollInput: UpdatePollInput };
+} = (userId, pollId, updatePollInput) => ({
   type: actionTypes.UPDATE_POLL_LOADING,
   payload: { userId, pollId, updatePollInput }
 });
-export const getUserData: ActionCreator<Action> = () => ({
+
+export const getUserData: () => Action = () => ({
   type: actionTypes.GET_USER_DATA_LOADING
 });
