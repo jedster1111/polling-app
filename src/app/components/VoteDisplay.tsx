f7b3ac47fe1b38bd2192bdca6bbba6a0e56671f6
Jed Thompson <jedster1111@hotmail.co.uk>
2018-10-18 15:51:42 +0100

    Added styling to show your votes total is greater than the limit

--- src/app/components/VoteDisplay.tsx
+++ src/app/components/VoteDisplay.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Poll, User } from "../types";
 
 interface VoteDisplayProps {
@@ -6,16 +7,27 @@ interface VoteDisplayProps {
   user: User;
 }
 
-const VoteDisplay: React.SFC<VoteDisplayProps> = props => (
-  <p>
-    Votes:{" "}
-    {
-      props.poll.options.filter(option =>
-        option.votes.find(vote => vote.id === props.user.id)
-      ).length
-    }{" "}
-    / {props.poll.voteLimit}
-  </p>
-);
+const VoteDisplayContainer = styled.div<{
+  numberOfVotes: number;
+  voteLimit: number;
+}>`
+  color: ${({ numberOfVotes, voteLimit }) =>
+    numberOfVotes > voteLimit ? "red" : "black"};
+`;
+
+const VoteDisplay: React.SFC<VoteDisplayProps> = props => {
+  const numberOfVotes = props.poll.options.filter(option =>
+    option.votes.find(vote => vote.id === props.user.id)
+  ).length;
+
+  return (
+    <VoteDisplayContainer
+      numberOfVotes={numberOfVotes}
+      voteLimit={props.poll.voteLimit}
+    >
+      Votes: {numberOfVotes} / {props.poll.voteLimit}
+    </VoteDisplayContainer>
+  );
+};
 
 export default VoteDisplay;
