5643e30db9861256e72304ddc74b6e6f4a896613
Jed <jedster1111@hotmail.co.uk>
2018-12-09 10:27:07 +0000

    Trying to get LOCAL_IP as an env var inside of container

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ if (ENV === "development") {
 
 // const rootUrl = process.env.URL || "127.0.0.1:8000";
 const BASE_URL =
-  `http://${process.env.TESTCAFE_IP}:8000` || "http://127.0.0.1:8000";
+  `http://${process.env.LOCAL_IP}:8000` || "http://127.0.0.1:8000";
 const secretKey = process.env.SECRET_KEY || "SuperSecretKey";
 const clientId = process.env.CLIENT_ID || "GithubProvidedClientId";
 const clientSecret = process.env.CLIENT_SECRET || "GithubProvidedSecretKey";
